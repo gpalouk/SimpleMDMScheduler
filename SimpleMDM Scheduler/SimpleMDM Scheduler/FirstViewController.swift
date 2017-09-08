5579d1ce6324f517fea6a89380ca7ca28484b637
George Paloukis <g.paloukis@intelen.com>
2017-09-08 18:05:09 +0300

    XCode project initialization

---
+++ SimpleMDM Scheduler/SimpleMDM Scheduler/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SimpleMDM Scheduler
+//
+//  Created by George Paloukis on 08/09/2017.
+//  Copyright © 2017 Mobilergon. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
